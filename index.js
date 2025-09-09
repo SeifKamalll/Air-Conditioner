253ebbaab834862de5f07059b1e29c3934038f82
Seif Kamal <seifkemos@gmail.com>
2025-09-09 06:16:52 +0300

    Air Conditioner

---
+++ index.js
@@ -0,0 +1,39 @@
+
+let acstatus = false;
+let fanspeed = 0;
+let temp = 16;
+
+function status() {
+  if (acstatus == false) {
+    acstatus = true;
+  } else {
+    acstatus = false;
+  }
+  console.log("Status is : " + acstatus);
+}
+
+
+function speed() {
+  if (fanspeed < 3 && acstatus == true) {
+    fanspeed++;
+  } else if (fanspeed == 3 && acstatus == true) {
+    fanspeed = 1;
+  }
+  console.log("Fan Speed is : " + fanspeed);
+}
+
+
+function up() {
+  if (temp < 29 && acstatus == true) {
+    temp++;
+  }
+  console.log("Temperature is : " + temp);
+}
+
+
+function down() {
+  if (temp > 16 && acstatus == true) {
+    temp--;
+  }
+  console.log("Temperature is : " + temp);
+}
